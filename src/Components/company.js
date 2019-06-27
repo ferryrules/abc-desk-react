69cab7aa803c9f77b9d0203bcf0580ebb7441c59
ferryrules <ferris@ferryrules.com>
2019-06-27 12:02:19 -0400

    list employees

--- src/Components/company.js
+++ src/Components/company.js
@@ -28,15 +28,21 @@ class Company extends React.Component {
   render() {
     console.log(this.props);
     const eachCompany = this.state.companies.map(c=>{
-      // debugger
-      return <Card key={c.name} onClick={(e)=>{console.log(e.currentTarget.id)}} id={c.id}>
-        <Card.Content>
-          <Card.Header>{c.name}</Card.Header>
-          <Card.Description><List>{c.employees.map(e=>{
-            return <List.Item>{e.full_name}</List.Item>
-          })}</List></Card.Description>
-        </Card.Content>
-      </Card>
+      return (
+        <Card key={c.name} onClick={(e)=>{console.log(e.currentTarget.id)}} id={c.id}>
+          <Card.Content>
+            <Card.Header>{c.name}</Card.Header>
+              <Card.Description>
+                <List>{c.employees.map(e=>{
+                  return <List.Item key={e.id}>{e.full_name}</List.Item>
+                }).sort((a,b)=>{
+                  return a.props.children.localeCompare(b.props.children)
+                })
+              }</List>
+            </Card.Description>
+          </Card.Content>
+        </Card>
+      )
     })
     return (
       <div className="ui cards">
