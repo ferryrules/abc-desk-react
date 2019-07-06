86ec81008b6c8d0a6d8650f89f4961dbbf8bbcd3
ferryrules <ferris@ferryrules.com>
2019-07-06 15:01:16 -0400

    app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { Route, Switch, Redirect, withRouter } from 'react-router-dom'
 import Profile from './containers/profile'
 import LoginForm from './components/loginForm'
 import Nav from './containers/nav'
-import Dashboard from './containers/dashboard'
+import CompanyDashboard from './containers/companyDashboard';
 
 import Company from './components/company'
 import CompaniesList from './containers/companiesList'
 
 import Ticket from './components/ticket'
+import TicketsList from './containers/ticketsList'
 import TicketForm from './forms/ticketForm'
 
 import EmployeesList from './containers/employeesList'
@@ -17,6 +18,7 @@ import EmployeeForm from './forms/employeeForm'
 
 import PayrollForm from './forms/payrollForm'
 import Payroll from './components/payroll'
+import PayrollsList from './containers/payrollsList'
 
 import NotFound from './components/notFound'
 import './App.css'
@@ -25,11 +27,11 @@ class App extends React.Component {
 
   state ={
     companies: [],
-    currentCompany: []
+    company: []
   }
 
   componentDidMount() {
-    fetch('http://localhost:3000/companies', {
+    fetch("http://localhost:3000/companies", {
       method: 'GET',
       headers: {
         'Authorization': `Bearer ${localStorage.getItem('jwt')}`
@@ -37,46 +39,39 @@ class App extends React.Component {
     })
     .then(r=>r.json())
     .then(companies=>{
+      // let comp = companies.length > 1 ? 'companies' : 'company'
       this.setState({
-        companies
+        company: companies[0]
       })
     })
+    .catch(error=> console.error('Error', error))
   }
 
-  selectCompany = (company) => {
-    this.setState({
-      currentCompany: company
-    })
-  }
-
-  // console.log('%c APP Props: ', 'color: firebrick', props)
-  // <Route exact path="/companies/:company_id/employees/:employee_id" render={()=><Employee {...props}/>} />
   render() {
     // console.log("App state",this.state);
     return (
       <Fragment>
-        <Nav currentCompany={this.state.currentCompany}/>
+        <Nav company={this.state.company}/>
         <Switch>
           <Route exact path="/" render={() => <Redirect to="/dashboard" />} />
           <Route exact path="/profile" component={Profile} />
           <Route exact path="/login" component={LoginForm} />
-          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/dashboard" component={CompanyDashboard} />
 
-          <Route exact path="/companies" render={()=><CompaniesList {...this.props} {...this.state} selectCompany={this.selectCompany} />} />
-          <Route exact path="/companies/:company_id" render={()=><Company {...this.props} {...this.state}/>} />
+          <Route exact path="/:company_name" render={()=><Company {...this.props} company={this.state.company}/>} />
 
-          <Route exact path="/tickets" render={()=><TicketForm {...this.props} {...this.state}/>} />
-          <Route exact path="/tickets/:ticket_id" render={()=><Ticket {...this.props} {...this.state}/>} />
-          <Route exact path="/tickets/:ticket_id/edit" render={()=><TicketForm {...this.props} {...this.state}/>} />
+          <Route exact path="/tickets" render={()=><TicketsList {...this.props} company={this.state.company}/>} />
+          <Route exact path="/tickets/:ticket_id" render={()=><Ticket {...this.props} company={this.state.company}/>} />
+          <Route exact path="/tickets/:ticket_id/edit" render={()=><TicketForm {...this.props} company={this.state.company}/>} />
 
-          <Route exact path="/employees" render={()=><EmployeesList {...this.props} {...this.state}/>} />
-          <Route exact path="/employees/:employee_id" render={()=><Employee {...this.props} {...this.state}/>} />
-          <Route exact path="/employees/:employee_id/edit" render={()=><EmployeeForm {...this.props} {...this.state}/>} />
-          <Route exact path="/employees/new" render={()=><EmployeeForm {...this.props} {...this.state}/>} />
+          <Route exact path="/employees" render={()=><EmployeesList {...this.props} company={this.state.company}/>} />
+          <Route exact path="/employees/:employee_id" render={()=><Employee {...this.props} company={this.state.company}/>} />
+          <Route exact path="/employees/:employee_id/edit" render={()=><EmployeeForm {...this.props} company={this.state.company}/>} />
+          <Route exact path="/employees/new" render={()=><EmployeeForm {...this.props} company={this.state.company}/>} />
 
-          <Route exact path="/payrolls" render={()=><PayrollForm {...this.props} {...this.state}/>} />
-          <Route exact path="/payrolls/:payroll_id" render={()=><Payroll {...this.props} {...this.state}/>} />
-          <Route exact path="/payrolls/:payroll_id/edit" render={()=><PayrollForm {...this.props} {...this.state}/>} />
+          <Route exact path="/payrolls" render={()=><PayrollForm {...this.props} company={this.state.company}/>} />
+          <Route exact path="/payrolls/:payroll_id" render={()=><Payroll {...this.props} company={this.state.company}/>} />
+          <Route exact path="/payrolls/:payroll_id/edit" render={()=><PayrollForm {...this.props} company={this.state.company}/>} />
 
           <Route component={NotFound} />
         </Switch>
@@ -86,3 +81,6 @@ class App extends React.Component {
 }
 
 export default withRouter(App)
+
+
+// <Route exact path="/companies" render={()=><CompaniesList {...this.props} {...this.state} selectCompany={this.selectCompany} />} />
