eb8298d55c7f6a72f765a286252a2d855b1064b1
ferryrules <ferris@ferryrules.com>
2019-07-08 09:22:33 -0400

    edit user

---
+++ src/forms/userForm.js
@@ -0,0 +1,118 @@
+import React from 'react'
+// import { Card } from 'semantic-ui-react'
+// u1 = User.create(email: 'ferris@ferryrules.com', username: 'ferryrules', password_digest: 'asd', permission: 'Super Admin', fname: 'Ferris', lname: 'Boran')
+class UserForm extends React.Component {
+
+  state = {
+    fname: '',
+    lname: '',
+    permission: '',
+    password_digest: '',
+    username: '',
+    email: ''
+  }
+
+  componentDidMount() {
+    if (this.props.user) {
+      this.setState({
+        fname: this.props.user.fname,
+        lname: this.props.user.lname,
+        permission: this.props.user.permission,
+        password_digest: this.props.user.password_digest,
+        username: this.props.user.username,
+        email: this.props.user.email
+      })
+    }
+  }
+
+  handleChange = (e) => {
+    const {name,value} = e.target
+    this.setState({
+      [name]: value
+    })
+  }
+
+  cancelSubmit = () => {
+    this.props.props.history.push(`/companies`)
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    if (!this.state.fname) {
+      window.confirm(`Please enter a name`)
+    } else {
+      !!this.props.user
+      ? this.fetFunc(`http://localhost:3000/users/${this.props.user.id}`, 'PATCH',user=>{
+        this.props.props.history.push(`/companies/${this.state.company_id}`)
+      })
+      : this.fetFunc(`http://localhost:3000/users`, 'POST', user=>{
+        this.props.props.history.push(`/users/${user.id}`)
+      })
+    }
+  }
+
+  render() {
+    // console.log("empForm state", this.state)
+    console.log("userForm props", this.props)
+    // console.log("empForm comp", this.props.company)
+
+    return(
+      <div className="ui equal width form">
+        <label className="ui h3">{this.props.user.username}</label>
+        <div className="fields">
+          <div className="field">
+            <label>First Name</label>
+            <input onChange={this.handleChange} type="text" name="fname" placeholder="Full Name" value={this.state.fname} />
+          </div>
+          <div className="field">
+            <label>Last Name</label>
+            <input onChange={this.handleChange} type="text" name="lname" placeholder="Salary or Hourly" value={this.state.lname} />
+          </div>
+          <div className="field">
+            <label>Permission</label>
+            <input onChange={this.handleChange} type="number" placeholder="Pay Rate" name="permission" value={this.state.permission}/>
+          </div>
+        </div>
+        <div className="fields">
+          <div className="field">
+            <label>Username</label>
+            <input onChange={this.handleChange} type="text" placeholder="Username" name="username" value={this.state.username}/>
+          </div>
+          <div className="field">
+            <label>Password</label>
+            <input onChange={this.handleChange} type="password" placeholder="Password" name="password_digest" value={this.state.password_digest}/>
+          </div>
+          <div className="field">
+            <label>E-mail</label>
+            <input onChange={this.handleChange} type="text" placeholder="E-mail" name="email" value={this.state.email}/>
+          </div>
+        </div>
+        <button className="ui positive basic button" type="submit" onClick={(e)=>this.handleSubmit(e)}><i className="save icon"></i>Save</button>
+        <button className="ui negative basic button" onClick={(e)=>this.cancelSubmit()}><i className="undo icon"></i>Cancel</button>
+      </div>
+    )
+  }
+
+  fetFunc = (url, method, then) => {
+    fetch(url, {
+      method: method,
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('jwt')}`,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        'fname': this.state.fname,
+        'lname': this.state.lname,
+        'permission': this.state.permission,
+        'password_digest': this.state.password_digest,
+        'username': this.state.username,
+        'email': this.state.email
+      })
+    })
+    .then(r=>r.json())
+    .then(then)
+  }
+}
+
+export default UserForm
